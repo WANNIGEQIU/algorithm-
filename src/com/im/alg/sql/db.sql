17de19ec70622c8e9788244553197f2387ccaa03
玩你个球儿 <wannigeqiuer@gmail.com>
2020-05-30 21:25:41 +0800

    continue....

--- src/com/im/alg/sql/db.sql
+++ src/com/im/alg/sql/db.sql
@@ -50,4 +50,43 @@ where stu_id not in
                    inner join course c on sc.sc_cid = c.c_id
                    inner join teacher t on c.t_id = t.t_id
           where t.t_name = "张三"
-      )
+      );
+
+-- 6. 查询学过张三老师所教的所有课程的同学的学号,姓名(重点)
+
+select s.stu_id "学号", s.stu_name "姓名" from student s
+where s.stu_id in (
+        select  sc.s_Id from score sc
+    inner join course c on sc.sc_cid = c.c_id
+    inner join teacher t on c.t_id = t.t_id where t_name = "张三"
+    );
+
+-- 7. 查询学过编号为1的课程并且也学过编号为2的课程的学生学号和姓名(重点)
+
+select stu_id,stu_name from student where stu_id
+in (
+                               select a.s_Id
+                                from (
+                                          (select * from score a where a.sc_cid = 1) a
+                                         inner join
+                                    ( select *  from score b where b.sc_cid = 2 ) b
+                                        on a.s_Id = b.s_Id
+                                    )
+                            );
+
+-- 8. 查询02课程的总成绩
+select sum(sc_score) from score group by sc_cid having sc_cid =2;
+
+-- 9. 查询所有课程成绩小于60分的学生的学号,姓名
+
+select distinct st.stu_id,st.stu_name from student st left join score sc
+on st.stu_id = sc.s_Id
+where sc.sc_score < 60 or sc.s_Id is null;
+
+-- 10. 查询没有学全所有课程的学生学号和姓名(重点)
+
+select st.stu_id,st.stu_name from student st left join score sc
+on st.stu_id = sc.s_Id  group by st.stu_id, st.stu_name
+having count(sc.sc_cid) < (select count(c_id) from course);
+
+
